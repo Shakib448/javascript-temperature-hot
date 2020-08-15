18c19f81539fd7741c56ef84b8b2874ab07dbd0d
Shakib448 <shakiba448@gmail.com>
2020-08-15 13:39:27 +0600

    Javascript Temperature

--- temperature.js
+++ temperature.js
@@ -1,6 +1,5 @@
 const API_KEY = "d8145f1c9f1913e2512cd701c771e6fc"
 
-
 function getWeather(){
     const city = document.getElementById('input-city').value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
@@ -9,8 +8,16 @@ function getWeather(){
         console.log(data);
         document.getElementById('temperature').innerHTML = data.main.temp;
         document.getElementById('city').innerHTML = data.name;
-        if(data.main.temp > 10){
-            document.getElementById('status').innerHTML = "very Hot";
+        if(data.main.temp < 15){
+            document.getElementById('status').innerHTML = "Cold";
+        }else if(data.main.temp < 19){
+            document.getElementById('status').innerHTML = "Cool";
+        }else if(data.main.temp < 27){
+            document.getElementById('status').innerHTML = "Rainy";
+        }else if(data.main.temp < 35){
+            document.getElementById('status').innerHTML = "Hot";
+        }else if(data.main.temp < 50){
+            document.getElementById('status').innerHTML = "Very Hot";
         }
     })
 
