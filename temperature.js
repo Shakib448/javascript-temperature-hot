43199c647c09dfd243f9fa2a7110bffb882f94c0
Shakib448 <shakiba448@gmail.com>
2020-08-15 22:31:42 +0600

    Javascript Temperature

--- temperature.js
+++ temperature.js
@@ -6,10 +6,11 @@ function getWeather(){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
     .then(res => res.json())
     .then(data => {
+        console.log(data)
         document.getElementById('temperature').innerHTML = data.main.temp.toFixed(0) + ' ' + `&deg;C`;
         document.getElementById('city').innerHTML = data.name;
         document.getElementById('status').innerHTML = data.weather[0].main
-        const icon = ("<img src='https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png'>")
+        const icon = ("<img src='https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png'>"); 
         document.getElementById('img').innerHTML = icon
     }).catch(()=>{
         alert("Please write the valid city name")
